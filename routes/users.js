b9f46133047bab90491e8751506d0dd6ede90afe
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-05-28 20:38:39 -0400

    Add User model and import it into routes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User')
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
 router.post('/', (req, res) => { 
-    res.send('Register a user')
+    // res.send('Register a user')
+    
 });
 
 module.exports = router;
