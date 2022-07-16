b28d98902eef500b605209b1092a7d9b626a5a6c
jazad136 <1129384+jazad136@users.noreply.github.com>
2022-07-16 08:59:12 -0400

    [SIMPLE] (#3) Version with checks commented out

--- routes/users.js
+++ routes/users.js
@@ -6,22 +6,22 @@ const {check, validationResult } = require('express-validator')
 // @desc    Register a user
 // @access  Public
 router.post(
-  "/",
-  [
-    check("name", "name is required").not().isEmpty(),
-    check("email", "Please include a valid email").isEmail(),
-    check(
-      "password",
-      "Please enter a password with 6 or more characters"
-    ).isLength({ min: 6 }),
-  ],
+  "/" , 
+  // [
+  //   check("name", "name is required").not().isEmpty(),
+  //   check("email", "Please include a valid email").isEmail(),
+  //   check(
+  //     "password",
+  //     "Please enter a password with 6 or more characters"
+  //   ).isLength({ min: 6 }),
+  // ],
   (req, res) => {
-    // res.send('Register a user')
-    var errors = validationResult(req)
-    console.log(errors.mapped())
-    if(!errors.isEmpty()) {
-        res.status(422).json({errors: errors.array()[0].msg})
-    }
+    // // res.send('Register a user')
+    // var errors = validationResult(req)
+    // console.log(errors.mapped())
+    // if(!errors.isEmpty()) {
+    //     res.status(422).json({errors: errors.array()[0].msg})
+    // }
     res.send(req.body);
   }
 );
